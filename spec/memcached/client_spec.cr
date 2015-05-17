270211d92fd5fb07195e67b20c68b83d6bd95df0
Dmitry Rybakov <dmitry.rybakov@gmail.com>
2015-05-17 13:22:02 +0200

    Response class; logging

--- spec/memcached/client_spec.cr
+++ spec/memcached/client_spec.cr
@@ -2,13 +2,13 @@ require "spec"
 
 describe Memcached::Client do
   it "sets and then gets" do
-    server = Memcached::Client.new
-    server.set("Hello", "World").should eq(true)
-    server.get("Hello").should eq("World")
+    client = Memcached::Client.new
+    client.set("Hello", "World").should eq(true)
+    client.get("Hello").should eq("World")
   end
 
   it "does not get non existing key" do
-    server = Memcached::Client.new
-    server.get("SomeStrangeKey").should eq(nil)
+    client = Memcached::Client.new
+    client.get("SomeStrangeKey").should eq(nil)
   end
 end
