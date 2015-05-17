52154ac568e98c8447e43980b6f136e95087c788
Dmitry Rybakov <dmitry.rybakov@gmail.com>
2015-05-17 16:55:06 +0200

    Refactoring

--- src/memcached/client.cr
+++ src/memcached/client.cr
@@ -31,58 +31,26 @@ module Memcached
     end
 
     def set(key : String, value : String)
-      extras_length = 8_u8
-      key_bytes = key.bytes
-      key_length = key_bytes.size.to_u16
-      value_bytes = value.bytes
-      total_length = (key_bytes.size + value_bytes.size + extras_length).to_u32
-      # Write header
-      @io.write([
-        MAGICS["request"],                              # magic 0
-        OPCODES["set"],                                 # opcode 1
-        ((key_length >> 8) & 0xFF).to_u8,               # key length 2
-        (key_length & 0xFF).to_u8,                      # key length 3
-        extras_length,                                  # extra length 4
-        0_u8                                            # data type 5
-        0_u8, 0_u8,                                     # vbucket 6, 7
-        ((total_length >> 24) & 0xFF).to_u8             # total body 8
-        ((total_length >> 16) & 0xFF).to_u8             # total body 9
-        ((total_length >> 8) & 0xFF).to_u8              # total body 10
-        (total_length & 0xFF).to_u8                     # total body 11
-        0_u8, 0_u8, 0_u8, 0_u8,                         # opaque 12, 13, 14, 15
-        0_u8, 0_u8, 0_u8, 0_u8, 0_u8, 0_u8, 0_u8, 0_u8, # cas 16, 17, 18, 19, 20, 21, 22, 23
-        0xde_u8, 0xad_u8, 0xbe_u8, 0xef_u8,             # flags 24, 25, 28, 27
-        0x00_u8, 0x00_u8, 0x00_u8, 0x00_u8              # expire 28, 29, 30, 31
-      ])
-      # Write body
-      @io.write(key_bytes)
-      @io.write(value_bytes)
+      send_request(
+        OPCODES["set"],
+        key.bytes,
+        value.bytes,
+        [
+          0xde_u8, 0xad_u8, 0xbe_u8, 0xef_u8,
+          0x00_u8, 0x00_u8, 0x00_u8, 0x00_u8
+        ]
+      )
       @io.flush
       read_response.try { |response| response.successful? }
     end
 
     def get(key : String)
-      key_bytes = key.bytes
-      key_length = key_bytes.size.to_u16
-      total_length = (key_bytes.size).to_u32
-      # Write header
-      @io.write([
-        MAGICS["request"],                              # magic 0
-        OPCODES["get"],                                 # opcode 1
-        ((key_length >> 8) & 0xFF).to_u8,               # key length 2
-        (key_length & 0xFF).to_u8,                      # key length 3
-        0x00_u8,                                        # extra length 4
-        0_u8                                            # data type 5
-        0_u8, 0_u8,                                     # vbucket 6, 7
-        ((total_length >> 24) & 0xFF).to_u8             # total body 8
-        ((total_length >> 16) & 0xFF).to_u8             # total body 9
-        ((total_length >> 8) & 0xFF).to_u8              # total body 10
-        (total_length & 0xFF).to_u8                     # total body 11
-        0_u8, 0_u8, 0_u8, 0_u8,                         # opaque 12, 13, 14, 15
-        0_u8, 0_u8, 0_u8, 0_u8, 0_u8, 0_u8, 0_u8, 0_u8  # cas 16, 17, 18, 19, 20, 21, 22, 23
-      ])
-      # Write body
-      @io.write(key_bytes)
+      send_request(
+        OPCODES["get"],
+        key.bytes,
+        Array(UInt8).new(0),
+        Array(UInt8).new(0)
+      )
       @io.flush
       read_response.try do |response|
         if response.successful?
@@ -97,42 +65,19 @@ module Memcached
       result = Hash(String, String | Nil).new
       keys.each do |key|
         result[key] = nil
-        key_bytes = key.bytes
-        key_length = key_bytes.size.to_u16
-        total_length = (key_bytes.size).to_u32
-        @io.write([
-          MAGICS["request"],                              # magic 0
-          OPCODES["getkq"],                               # opcode 1
-          ((key_length >> 8) & 0xFF).to_u8,               # key length 2
-          (key_length & 0xFF).to_u8,                      # key length 3
-          0x00_u8,                                        # extra length 4
-          0_u8                                            # data type 5
-          0_u8, 0_u8,                                     # vbucket 6, 7
-          ((total_length >> 24) & 0xFF).to_u8             # total body 8
-          ((total_length >> 16) & 0xFF).to_u8             # total body 9
-          ((total_length >> 8) & 0xFF).to_u8              # total body 10
-          (total_length & 0xFF).to_u8                     # total body 11
-          0_u8, 0_u8, 0_u8, 0_u8,                         # opaque 12, 13, 14, 15
-          0_u8, 0_u8, 0_u8, 0_u8, 0_u8, 0_u8, 0_u8, 0_u8  # cas 16, 17, 18, 19, 20, 21, 22, 23
-        ])
-        # Write body
-        @io.write(key_bytes)
+        send_request(
+          OPCODES["getkq"],
+          key.bytes,
+          Array(UInt8).new(0),
+          Array(UInt8).new(0)
+        )
       end
-      @io.write([
-        MAGICS["request"],                              # magic 0
-        OPCODES["noop"],                                # opcode 1
-        0.to_u8,                                        # key length 2
-        0.to_u8,                                        # key length 3
-        0x00_u8,                                        # extra length 4
-        0_u8                                            # data type 5
-        0_u8, 0_u8,                                     # vbucket 6, 7
-        0.to_u8                                         # total body 8
-        0.to_u8                                         # total body 9
-        0.to_u8                                         # total body 10
-        0.to_u8                                         # total body 11
-        0_u8, 0_u8, 0_u8, 0_u8,                         # opaque 12, 13, 14, 15
-        0_u8, 0_u8, 0_u8, 0_u8, 0_u8, 0_u8, 0_u8, 0_u8  # cas 16, 17, 18, 19, 20, 21, 22, 23
-      ])
+      send_request(
+        OPCODES["noop"],
+        Array(UInt8).new(0),
+        Array(UInt8).new(0),
+        Array(UInt8).new(0)
+      )
       @io.flush
       while response = read_response
         Memcached.logger.info(String.new(response.body))
@@ -176,6 +121,37 @@ module Memcached
         @socket.read(body)
       end
       Response.new(status_code, opcode, key_length, body, extras)
+    end
+
+    private def send_request(opcode : UInt8, key : Array(UInt8), value : Array(UInt8), extras : Array(UInt8))
+      extras_length = extras.length.to_u8
+      key_length = key.length.to_u16
+      total_length = (key.length + value.length + extras_length).to_u32
+      # Write header
+      @io.write([
+        MAGICS["request"],                              # magic 0
+        opcode,                                         # opcode 1
+        ((key_length >> 8) & 0xFF).to_u8,               # key length 2
+        (key_length & 0xFF).to_u8,                      # key length 3
+        extras_length,                                  # extra length 4
+        0_u8                                            # data type 5
+        0_u8, 0_u8,                                     # vbucket 6, 7
+        ((total_length >> 24) & 0xFF).to_u8             # total body 8
+        ((total_length >> 16) & 0xFF).to_u8             # total body 9
+        ((total_length >> 8) & 0xFF).to_u8              # total body 10
+        (total_length & 0xFF).to_u8                     # total body 11
+        0_u8, 0_u8, 0_u8, 0_u8,                         # opaque 12, 13, 14, 15
+        0_u8, 0_u8, 0_u8, 0_u8, 0_u8, 0_u8, 0_u8, 0_u8, # cas 16, 17, 18, 19, 20, 21, 22, 23
+      ])
+      if extras.length > 0
+        @io.write(extras)
+      end
+      if key.length > 0
+        @io.write(key)
+      end
+      if value.length > 0
+        @io.write(value)
+      end
     end
   end
 end
